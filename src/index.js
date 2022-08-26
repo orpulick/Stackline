bddc1b7c910f36a78c852ef08d6d38a694659718
orpulick <o_pulick@yahoo.com>
2022-08-26 07:17:26 -0600

    Deleted web vitals component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./Store";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
@@ -19,4 +18,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
